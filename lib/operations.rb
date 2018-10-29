21f6968c42a5833cb1a98e11f1d30f5c22fd3086
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-29 03:37:36 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+if ((speed == speed > 60) && (speed == speed < 40))
+	return true
+else (speed == speed.between?(40,60)
+	 return false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
